72e2a3744cb2c7a25e79910a2e990bef0bfd8bd4
Ramesh Raja <118657318+RameshRaja05@users.noreply.github.com>
2023-09-08 18:09:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ name="Ramesh"
 
 def greeting():
   username=input("enter your username: ")
-  return f"Hi there,{username} from {name}"
+  return f"Hi there,{username} welcome to our App. Here is {Ramesh} who is a founder of this app"
 
 print(greeting())
